51e69d4342195906afd0503e45da8a2743185a57
prodbysky <kyrbynyt@gmail.com>
2023-12-13 08:22:20 +0200

    Rewritten code to use a map instead of a array

--- src/main.go
+++ src/main.go
@@ -12,22 +12,10 @@ import (
 	"time"
 	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
-	"github.com/prodbysky/sh-auth/pkg/models"
 )
 
-var users []models.User
-
-func removeDuplicates(s []models.User) []models.User {
-   bucket := make(map[models.User]bool)
-   var result []models.User
-   for _, usr := range s {
-      if _, ok := bucket[usr]; !ok {
-         bucket[usr] = true
-         result = append(result, usr)
-      }
-   }
-   return result
-}
+type Users map[string] string
+var users Users
 
 // Router related functions
 func authHash() string {
@@ -53,46 +41,19 @@ func getUser(c *gin.Context) {
 		return
 	}
 
-	user := models.User{Name: name, Password: pass}
-
-    users = append(users, user)
-    
-    fmt.Println(users)
+    _, ok := users[pass]
+    if ok {
+        c.JSON(http.StatusForbidden, gin.H{
+            "status": "user already exists",
+        }) 
+        return
+    }    
+    users[pass] = name
 
 	c.JSON(http.StatusOK, gin.H{
 		"status": "ok",
-		"user":   fmt.Sprintf("User: %s, password: %s", user.Name, user.Password),
+		"user":   fmt.Sprintf("User: %s, password: %s", name, pass),
 	})
-}
-
-// Data management functions
-func readUsers(filename string, users *[]models.User) {
-    data, err := os.ReadFile(filename)
-    if err != nil {
-        log.Fatalf("Non-existent filename provided %s\n", filename)
-    }
-
-    var result []models.User
-    err = json.Unmarshal(data, &result)
-    if err != nil {
-        log.Fatalln("Failed unmarshalling data")
-    }
-}
-
-func saveUsers(filename string, users []models.User) {
-    fmt.Println(users)
-    bytes, err := json.Marshal(users)
-    if err != nil {
-        log.Fatalln("Failed to marshal users:", err)
-    }
-
-    fmt.Println("Marshaled JSON:", string(bytes))
-
-    err = os.WriteFile(filename, bytes, 0644)
-    if err != nil {
-        log.Fatalln("Failed to save users to file:", err)
-    }
-
 }
 
 func main() {
@@ -114,7 +75,6 @@ func main() {
     endless.ListenAndServe(":8080", router)
 
     // Saving users
-    users = removeDuplicates(users)
     bytes, err := json.Marshal(users)
     if err != nil {
         log.Fatalln("Failed to marshal users:", err)
